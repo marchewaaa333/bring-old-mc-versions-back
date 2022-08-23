19f478e9d0ba264ad39c89a52f395165a465fc3f
33marchewka <87077877+marchewaaa333@users.noreply.github.com>
2022-08-23 13:07:00 +0200

    Update ModMain.java

--- Mod/src/me/marchewka/bringoldmc/ModMain.java
+++ Mod/src/me/marchewka/bringoldmc/ModMain.java
@@ -1,5 +1,25 @@
-package me.marchewka.bringoldmc;
+import org.lwjgl.opengl.Display;
 
-class ModMain {
-  
+import net.minecraft.client.Minecraft;
+
+public class ModMain {
+	public static Minecraft mc = Minecraft.getMinecraft();
+	public static final ModMain INSTANCE = new ModMain();
+	public static final ModMain getInstance() {
+		return INSTANCE;
+	}
+	private DiscordRP discordRP = new DiscordRP();
+	public DiscordRP getDiscordRP() {
+		return discordRP;
+	}
+	
+	public void start() {
+		Logger.log("Loading mod...");
+		discordRP.start();
+		mc.FRAME_CLIENT.setTitle("Minecraft 1.5.2 | Bring Old Mc Versions Back");
+	}
+	
+	public void shutdown() {
+		discordRP.shutdown();
+	}
 }
