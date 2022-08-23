b317355a3675c473ab37a438288dafda1055191b
33marchewka <87077877+marchewaaa333@users.noreply.github.com>
2022-08-23 15:33:55 +0200

    Update ModMain.java

--- Mod/src/me/marchewka/bringoldmc/ModMain.java
+++ Mod/src/me/marchewka/bringoldmc/ModMain.java
@@ -1,12 +1,21 @@
 package me.marchewka.bringoldmc;
 
-import org.lwjgl.opengl.Display;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import net.minecraft.client.Minecraft;
 
 public class ModMain {
 	public static Minecraft mc = Minecraft.getMinecraft();
 	Logger log = new Logger();
+	static JFrame CLIENT_UPDATE_FRAME;
 	public static final ModMain INSTANCE = new ModMain();
 	public static final ModMain getInstance() {
 		return INSTANCE;
@@ -17,13 +26,34 @@ public class ModMain {
 	}
 	
 	public void start() {
+		UpdateChecker.checkIfAvailable();
 		log.log("Loading Mod...");
+		log.log("IsUpdateAbailable? " + UpdateChecker.isAvailable());
+		if(UpdateChecker.isAvailable()) {
+			createAndShowUpdateGUI();
+		}else {
+			}
 		discordRP.start();
 		mc.FRAME_CLIENT.setTitle("Minecraft 1.5.2 | Bring Old Mc Versions Back");
 	}
 	
 	public void shutdown() {
-		log.log("Shutting down Mod...");
 		discordRP.shutdown();
 	}
+	
+    private static void createAndShowUpdateGUI() {
+    	  JFrame frame = new JFrame("Bring Old Mc Versions Back Updater!");  
+          JPanel panel = new JPanel();  
+          CLIENT_UPDATE_FRAME = frame;
+          panel.setLayout(new FlowLayout());  
+          JLabel label = new JLabel("<html><b>Please update your Mod Version!"
+          		+ "<br>download it here"
+          		+ "<br>https://github.com/marchewaaa333/bring-old-mc-versions-back</b></html>");   
+          panel.add(label);  
+          frame.add(panel);  
+          frame.setSize(500, 140);  
+          frame.setLocationRelativeTo(null);  
+          frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);  
+          frame.setVisible(true);  
+    }
 }
