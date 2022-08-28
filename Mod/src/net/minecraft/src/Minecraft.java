2eba45117e8392a9597aec4ebec5ebd79c5fc03a
33marchewka <87077877+marchewaaa333@users.noreply.github.com>
2022-08-28 13:20:32 +0200

    Create Minecraft.java

---
+++ Mod/src/net/minecraft/src/Minecraft.java
@@ -0,0 +1,9 @@
+/*160*/ public static Frame FRAME_CLIENT = new Frame();
+...
+/* 473*/        // Bring Old Mc Versions Back START
+/* 474*/        ModMain.getInstance().start();
+/* 475*/        // Bring Old Mc Versions Back END
+...
+ /*2264*/        // Bring Old Mc Versions Back START
+/*2265*/        FRAME_CLIENT = var15;
+/*2266*/        // Bring Old Mc Versions Back END
