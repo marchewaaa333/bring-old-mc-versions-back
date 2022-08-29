35bd7db4958c1557faed2f75dddf75df98341b8d
33marchewka <87077877+marchewaaa333@users.noreply.github.com>
2022-08-29 22:28:11 +0200

    Update Main.java

--- Launcher/Main.java
+++ Launcher/Main.java
@@ -1,1 +1,15 @@
+import javax.swing.*;  
 
+public class Main {  
+  
+    public static void main(String[] args) {  
+        JFrame f=new JFrame("BringOldMcLauncher");  
+        JButton b=new JButton("Launch");  
+        b.setBounds(50,100,95,30);  
+        f.add(b);  
+        f.setSize(400,400);  
+        f.setLayout(null);  
+        f.setVisible(true);   
+    }  
+  
+} 
